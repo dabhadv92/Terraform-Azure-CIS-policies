e0ead3118a6118f168c0c4d8adf3f0487fe9fb2b
Vivek <dabhadev92@gmail.com>
2024-02-24 22:47:52 +0530

    My commit

---
+++ terraform-manifests/c5-virtual-network.tf
@@ -0,0 +1,96 @@
+# Create Virtual Network
+
+/*
+resource "azurerm_virtual_network" "myvnet" {
+  name                = local.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  tags = local.common_tags
+}
+
+# Create Subnet
+resource "azurerm_subnet" "mysubnet" {
+  name                 = local.snet_name
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+*/
+/*
+# Create Public IP Address
+resource "azurerm_public_ip" "mypublicip" {
+  name                = local.pip_name
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  allocation_method   = "Static"
+  domain_name_label = "app1-${terraform.workspace}-${random_string.myrandom.id}"
+  tags = local.common_tags
+}
+
+# Create Network Interface
+resource "azurerm_network_interface" "myvmnic" {
+  name                = local.nic_name
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mysubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.mypublicip.id 
+  }
+  tags = local.common_tags
+}
+
+*/
+
+resource "azurerm_virtual_network" "myvnet" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  
+}
+
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "Subnet-59"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "NSG" {
+  name = "NSG59"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location = azurerm_resource_group.myrg.location
+
+  security_rule {
+    name                       = "Rule-1"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.2.0/24"
+  }
+
+/*
+  security_rule {
+    name                       = "Rule-1"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "*"
+  }
+
+*/
+  
+}
