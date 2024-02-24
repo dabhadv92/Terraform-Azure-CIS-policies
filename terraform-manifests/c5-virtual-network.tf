f4d797ff822a55b07b73904221f2a13819ce3a44
Vivek <dabhadev92@gmail.com>
2024-02-24 23:09:39 +0530

    My-New-commit

--- terraform-manifests/c5-virtual-network.tf
+++ terraform-manifests/c5-virtual-network.tf
@@ -73,7 +73,7 @@ resource "azurerm_network_security_group" "NSG" {
     access                     = "Deny"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = ["3389", "22"]
     source_address_prefix      = "*"
     destination_address_prefix = "10.0.2.0/24"
   }
